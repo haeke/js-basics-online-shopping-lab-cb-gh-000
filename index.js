224a6469cd5af28b2f7075e846fe8f894da3a410
edwin rivera <waehake@gmail.com>
2018-01-18 03:09:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  if (cart.length <= 0) {
+  if (!cart.length) {
     console.log('Your shopping cart is empty.');
   }
 
