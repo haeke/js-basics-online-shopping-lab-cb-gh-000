7ecad5864adb72a71aa2c494fae588595dd0d464
edwin rivera <waehake@gmail.com>
2018-01-18 02:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart = new Object({ item: (Math.random() * 100) + 1 })
+ cart = new Object({ item: (Math.random() * 100) + 1 });
  return cart;
 }
 
