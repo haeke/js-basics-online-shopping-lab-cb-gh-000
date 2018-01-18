3689d614c0abad40edf829c64aa39323985873dc
edwin rivera <waehake@gmail.com>
2018-01-18 03:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,4 +68,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+    return cart;
+  } else {
+    return console.log("Sorry, we don\'t have a credit card on file for you.");
+  }
 }
